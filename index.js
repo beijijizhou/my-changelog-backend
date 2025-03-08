078084d3969b3b4e599e82ec23ca8612ca2e94be
“beijijizhou” <hongzhong.hu@stonybrook.edu>
2025-03-08 05:13:30 -0500

    update backend url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 dotenv.config()
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', ['http://localhost:5173','https://my-changelog-app.vercel.app']);
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });

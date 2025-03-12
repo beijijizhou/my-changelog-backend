0731abf00f47cd973190162f79840fb5ff7f97c7
“beijijizhou” <hongzhong.hu@stonybrook.edu>
2025-03-12 11:52:45 -0400

    fix: change get to post method

--- routes/commits/routes.js
+++ routes/commits/routes.js
@@ -3,5 +3,5 @@ import { commitMiddlewares, summarizeCommitMessages } from './middleware.js';
 import { getAllCommits, getCommitSummary } from './controller.js';
 const commitRoutes = express.Router();
 commitRoutes.get('/', commitMiddlewares, getAllCommits);
-commitRoutes.get('/summary', summarizeCommitMessages, getCommitSummary);
+commitRoutes.post('/summary', summarizeCommitMessages, getCommitSummary);
 export { commitRoutes };
